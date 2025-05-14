be284ad3e870ec257d3599a3ff4efec6181784cc
BowlPulp <kartikarora5832@gmail.com>
2025-05-15 00:13:43 +0530

    feat: Add @vercel/analytics dependency and integrate Analytics component in main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,11 +5,12 @@ import { BrowserRouter } from 'react-router-dom'
 // App.js or index.js
 import './styles/theme.css'; // adjust path if needed
 import ScrollToTop from './ScrollToTop.jsx';
-
+import { Analytics } from "@vercel/analytics/next"
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   <ScrollToTop/>
+  <Analytics/>
     <App />
   </BrowserRouter>,
 )

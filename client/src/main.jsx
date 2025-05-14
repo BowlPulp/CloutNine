0ed786a1d859b8816df9bd0cebf426c9585a4169
BowlPulp <kartikarora5832@gmail.com>
2025-05-15 00:20:38 +0530

    feat: Replace Analytics component with inject method from @vercel/analytics in main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,12 +5,13 @@ import { BrowserRouter } from 'react-router-dom'
 // App.js or index.js
 import './styles/theme.css'; // adjust path if needed
 import ScrollToTop from './ScrollToTop.jsx';
-import { Analytics } from "@vercel/analytics/next"
+import { inject } from '@vercel/analytics'
+
+inject()
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   <ScrollToTop/>
-  <Analytics/>
     <App />
   </BrowserRouter>,
 )
